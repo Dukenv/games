a0ed5d8e29cdfd974f69d208c8c760acf4c61021
Dukenv <62391010+Dukenv@users.noreply.github.com>
2023-12-31 13:32:14 +0900

    Add files via upload

--- WordsGame/scripts/word-list.js
+++ WordsGame/scripts/word-list.js
@@ -1,50 +1,50 @@
 const wordList = [
     {
-        word: "Tyrannosaurus",
+        word: "tyrannosaurus",
         hint: "This dinosaur is known as the 'king of the dinosaurs.' It has short arms but a very large head and sharp teeth."
-        },
-        {
-        word: "Stegosaurus",
+    },
+    {
+        word: "stegosaurus",
         hint: "This dinosaur has plates on its back and spikes on its tail. It's famous for its distinctive and unique appearance."
-        },
-        {
-        word: "Triceratops",
+    },
+    {
+        word: "triceratops",
         hint: "Imagine a dinosaur with three horns on its face, like a rhinoceros but from the prehistoric times."
-        },
-        {
-        word: "Velociraptor",
+    },
+    {
+        word: "velociraptor",
         hint: "This dinosaur is small and fast. It's often depicted as a clever predator with sharp claws."
-        },
-        {
-        word: "Brachiosaurus",
+    },
+    {
+        word: "brachiosaurus",
         hint: "Think of a dinosaur with a long neck that reaches high into the trees. It's a giant, long-necked herbivore."
-        },
-        {
-        word: "Pterodactyl",
+    },
+    {
+        word: "pterodactyl",
         hint: "This is not exactly a dinosaur but a flying reptile from the same era. It has a wingspan and beak like a bird."
-        },
-        {
-        word: "Ankylosaurus",
+    },
+    {
+        word: "ankylosaurus",
         hint: "Picture a dinosaur with armor-like plates and a clubbed tail. It's like a walking tank from the past."
-        },
-        {
-        word: "Diplodocus",
+    },
+    {
+        word: "diplodocus",
         hint: "Imagine a dinosaur with an extremely long neck and tail. It's a gentle giant, munching on leaves from tall trees."
-        },
-        {
-        word: "Parasaurolophus",
+    },
+    {
+        word: "parasaurolophus",
         hint: "This dinosaur has a crest on its head that looks like a long tube. It might remind you of a musical instrument."
-        },
-        {
-        word: "Allosaurus",
+    },
+    {
+        word: "allosaurus",
         hint: "Think of a large carnivorous dinosaur with sharp teeth and a powerful jaw. It was a fearsome predator in its time."
-        },
-        {
-        word: "Iguanodon",
+    },
+    {
+        word: "iguanodon",
         hint: "Imagine a dinosaur that resembles a modern iguana but much larger. It's a herbivore with thumb spikes."
-        },
-        {
-        word: "Spinosaurus",
+    },
+    {
+        word: "apinosaurus",
         hint: "This dinosaur is known for its sail-like structure on its back. It was a massive predator, even larger than T. rex."
-        },   
+    },   
 ];
